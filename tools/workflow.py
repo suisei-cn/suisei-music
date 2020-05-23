aa3042b64ecbdc30f83904cd62eecc4089b0f797
Daniel <daniel65536@gmail.com>
2020-05-23 21:20:21 +0800

    add clip function into workflow

--- tools/workflow.py
+++ tools/workflow.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from dotenv import load_dotenv
 from pathlib import Path
 import csv
 import logging
+import os
 import subprocess
 import xxhash
 
@@ -11,6 +13,7 @@ logging.basicConfig(
     level = logging.INFO,
     format = '%(asctime)s %(name)s %(levelname)s - %(message)s',
 )
+load_dotenv()
 
 class Music(object):
     __slots__ = [
@@ -32,7 +35,7 @@ class Music(object):
         self.video_id = item['video_id']
         self.clip_start = item['clip_start']
         self.clip_end = item['clip_end']
-        self.done = item['done']
+        self.done = item['done'] == 'TRUE'
         self.title = item['title']
         self.artist = item['artist']
         self.performer = item['performer']
@@ -50,7 +53,7 @@ class Music(object):
     def __repr__(self):
         return f'Music(id={self.video_id}, title={self.title}, key={self.file_key})'
 
-class Processor(object):
+class Action(object):
     def __init__(self):
         self.logger = logging.getLogger(type(self).__name__)
 
@@ -62,11 +65,11 @@ class Processor(object):
 
     def process(self, items):
         for item in filter(self.filter, items):
-            self.logger.debug(f'Process {item}')
+            self.logger.debug(f'process {item}')
             self.effect(item)
-            self.logger.debug(f'Finish {item}')
+            self.logger.debug(f'finish {item}')
 
-class MetadataLinter(Processor):
+class MetadataLinter(Action):
     def __init__(self):
         super().__init__()
         self.music_artist = {}
@@ -76,20 +79,63 @@ class MetadataLinter(Processor):
 
     def effect(self, item):
         if any(map(lambda x: x.strip() != x, [item.title, item.artist, item.performer])):
-            self.logger.error(f'Detect whitespace in metadata on {item}')
+            self.logger.error(f'detect whitespace in metadata on {item}')
             raise RuntimeError('metadata format check failed')
 
         if item.title in self.music_artist and item.artist != self.music_artist[item.title]:
-            self.logger.error(f'Detect inconsistent relationship on {item.title}')
+            self.logger.error(f'detect inconsistent relationship on {item.title}')
             raise RuntimeError('metadata relationship check failed')
         else:
             self.music_artist[item.title] = item.artist
 
+class YoutubeClipper(Action):
+    def __init__(self, format_code, source_dir, output_dir):
+        super().__init__()
+        self.format_code = format_code
+        self.source_dir = Path(source_dir)
+        self.output_dir = Path(output_dir)
+
+        self.source_ext = {140: 'mp4', 251: 'webm'}[format_code]
+        self.output_ext = {140: 'm4a', 251: 'ogg'}[format_code]
+
+    def filter(self, item):
+        return item.video_type == 'YOUTUBE'
+
+    def effect(self, item):
+        source_path = self.source_dir / f'{item.video_id}.{self.format_code}.{self.source_ext}'
+        output_path = self.output_dir / f'{item.file_key}.{self.output_ext}'
+
+        if not source_path.exists():
+            self.logger.info(f'download {source_path}')
+            cmd = [
+                '/usr/local/bin/youtube-dl',
+                '-f', self.format_code,
+                '-o', str(source_path),
+                f'https://www.youtube.com/watch?v={item.video_id}',
+            ]
+            subprocess.run(cmd, check=True, capture_output=True)
+
+        if item.done and not output_path.exists():
+            self.logger.info(f'clip {output_path} for {item}')
+            cmd = [
+                '/usr/local/bin/ffmpeg',
+                '-i', str(source_path),
+                '-acodec', 'copy', '-vn'
+            ]
+            if item.clip_start:
+                cmd += ['-ss', item.clip_start]
+            if item.clip_end:
+                cmd += ['-to', item.clip_end]
+            cmd.append(str(output_path))
+            subprocess.run(cmd, check=True, capture_output=True)
+
 def main():
     with open('../suisei-music.csv') as f:
-        items = set(map(Music, csv.DictReader(f)))
+        items = frozenset(map(Music, csv.DictReader(f)))
 
     MetadataLinter().process(items)
+    YoutubeClipper(251, os.getenv('SOURCE_DIR'), os.getenv('OUTPUT_DIR')).process(items)
+    YoutubeClipper(140, os.getenv('SOURCE_DIR'), os.getenv('OUTPUT_DIR')).process(items)
 
 if __name__ == '__main__':
     main()
